0410ebcb244ac07d6c1cc6f19d6d6e7fe9ab2674
JiazhengWu <jenny.wuu@live.ca>
2018-12-06 20:44:00 -0700

    adding a function to parse ids from master request

--- League C++ API/JsonParser.cpp
+++ League C++ API/JsonParser.cpp
@@ -2,9 +2,26 @@
 
 JsonParser::JsonParser()
 {
+	requestInterface = std::make_unique<CurlRequestInterface>();
 }
-
 
 JsonParser::~JsonParser()
 {
 }
+
+std::map<std::string, std::string> JsonParser::parseMasterIds(const std::string masterList)
+{
+	std::map<std::string, std::string> masterRoles;
+
+	if (masterList != "")
+	{
+		const char *masterListC = masterList.c_str();
+		if (!document.Parse(masterListC).HasParseError())
+		{
+			//TODO
+		}
+	}
+
+	return masterRoles;
+}
+
--- League C++ API/JsonParser.h
+++ League C++ API/JsonParser.h
@@ -1,13 +1,26 @@
 #ifndef JSON_PARSER_H
 #define JSON_PARSER_H
 
+#include "CurlRequestInterface.h"
+#include <rapidjson/document.h>
+#include <map>
+#include <iostream>
+#include <stdio.h>
+
+using namespace rapidjson;
+
 class JsonParser
 {
 public:
 	JsonParser();
 	~JsonParser();
 
+	std::map<std::string, std::string> parseMasterIds(const std::string masterList);
+
 private:
+	std::unique_ptr<CurlRequestInterface> requestInterface;
+
+	Document document;
 };
 
 
